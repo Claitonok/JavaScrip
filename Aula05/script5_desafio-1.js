4943e916a457f6c7ade7c82d2e04ad89411b64c7
CLAITON I <claiton.silva@uni9.edu.br>
2022-10-17 21:37:15 -0300

    commit
    Signed-off-by: CLAITON I <claiton.silva@uni9.edu.br>

--- Aula05/script5_desafio-1.js
+++ Aula05/script5_desafio-1.js
@@ -5,11 +5,14 @@ av2 = parseFloat(prompt("Digite a nota AV2"));
 media = (av1 + av2) / 2;
 
 saida.innerHTML += "media (6), Fique esperto <br>";
-saida.innerHTML += `AV1, nota ${av1} <br> AV2, nota ${av2} <br>`;
+saida.innerHTML += `AV1, nota = ${av1} <br> AV2, nota = ${av2} <br>`;
 saida.innerHTML += "";      
 
 if (media >= 6){
-    saida.innerHTML += "Aprovado - Parabéns ";
-}else{
-    saida.innerHTML += "reprovado - não foi dessa vez " ;
+    saida.innerHTML += "Aprovado - Parabéns <br>";
+    saida.innerHTML += `sua media foi : ${media}<br>`;
+}else {
+    saida.innerHTML += "reprovado <br>" ;
+    saida.innerHTML += `sua media foi : ${media}<br>`;
 }
+  
